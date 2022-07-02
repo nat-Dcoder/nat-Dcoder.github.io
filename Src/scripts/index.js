3a9fd54024b3cd1863c14011b4a191d13a3899a6
nat-Dcoder <nj433487@.com>
2022-07-02 09:02:26 +0000

    new code added

--- Src/scripts/index.js
+++ Src/scripts/index.js
@@ -1,20 +1,15 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-//Set the width & height of the canvas to match the viewport dimensions
-canvas.width = innerWidth;
-canvas.height = 300;
-
+const width = canvas.width = innerWidth;//Set the width & height of the canvas to match the viewport dimensions
+const height = canvas.height = 300;
 //-------------- Get access to the player sprite sheet ------------------------
 const playerObj = {
     //So the source position can be updated
     srcX : 0,
     srcY : 0,
-    //There are 6 different sprites on 1 rows
-    cols : 7,
-    rows : 2,
     //Work out the size of individual sprites because they are evenly spaced apart
-    spriteWidth : 302 / 7,
-    spriteHeight : 280 / 2,
+    frameWidth : 302 / 7,
+    frameHeight : 280 / 2,
     //So the animation can play
     totalFrames : 7, //Because there are 6 sprites in total. Therefore the animation will take place over 6 frames
     currentFrame : 0,
@@ -34,8 +29,8 @@ const bricksObj = {
     cols : 2,
     rows : 5,
     //Work out the size of individual sprites because they are evenly spaced apart
-    spriteWidth : 766 / 2,
-    spriteHeight : 737 / 8.6,
+    frameWidth : 766 / 2,
+    frameHeight : 737 / 8.6,
     //So the animation can play
     totalFrames : 2, //Because there are 6 sprites in total. Therefore the animation will take place over 6 frames
     currentFrame : 0,
@@ -55,8 +50,8 @@ const boxObj = {
     cols : 10,
     rows : 8,
     //Work out the size of individual sprites because they are evenly spaced apart
-    spriteWidth : 310 / 9.5,
-    spriteHeight : 312 / 10,
+    frameWidth : 310 / 9.5,
+    frameHeight : 312 / 10,
     //So the animation can play
     totalFrames : 10, //Because there are 6 sprites in total. Therefore the animation will take place over 6 frames
     currentFrame : 0,
@@ -72,12 +67,9 @@ const box2Obj = {
     //So the source position can be updated
     srcX : 0,
     srcY : 0,
-    //There are 6 different sprites on 1 rows
-    cols : 1,
-    rows : 1,
     //Work out the size of individual sprites because they are evenly spaced apart
-    spriteWidth : 100,
-    spriteHeight : 100,
+    frameWidth : 100,
+    frameHeight : 100,
     //So the animation can play
     totalFrames : 1, //Because there are 6 sprites in total. Therefore the animation will take place over 6 frames
     currentFrame : 0,
@@ -97,21 +89,21 @@ function animate() {
     requestAnimationFrame(animate); //The function will be called repeatedly on each new frame
 
     playerObj.currentFrame = playerObj.currentFrame % playerObj.totalFrames; //Work out the current frame of the animation. Remember that 0 counts as the first image of the animation.
-    playerObj.srcX = playerObj.currentFrame * playerObj.spriteWidth; //Src position is updated to show the new sprite image
+    playerObj.srcX = playerObj.currentFrame * playerObj.frameWidth; //Src position is updated to show the new sprite image
     //bricksObj.currentFrame = bricksObj.currentFrame % bricksObj.totalFrames; //Work out the current frame of the animation. Remember that 0 counts as the first image of the animation.
     //bricksObj.srcX = bricksObj.currentFrame * bricksObj.spriteWidth; //Src position is updated to show the new sprite image
 
     //image, srcX, srcY, srcWidth, srcHeight, destX, destY, destWidth, destHeight
     ctx.save();
     resizeImage();
-    ctx.drawImage(playerSpriteSheet, playerObj.srcX, playerObj.srcY, playerObj.spriteWidth, playerObj.spriteHeight, -150, -175, playerObj.spriteWidth, playerObj.spriteHeight);
-    ctx.drawImage(bricksSpriteSheet, bricksObj.srcX, bricksObj.srcY, bricksObj.spriteWidth, bricksObj.spriteHeight, -160, -55, bricksObj.spriteWidth, bricksObj.spriteHeight);
-    ctx.drawImage(bricksSpriteSheet, bricksObj.srcX, bricksObj.srcY, bricksObj.spriteWidth, bricksObj.spriteHeight, 100, -55, bricksObj.spriteWidth, bricksObj.spriteHeight);
-    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.spriteWidth, boxObj.spriteHeight, -4, 80, boxObj.spriteWidth, boxObj.spriteHeight);
-    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.spriteWidth, boxObj.spriteHeight, 65, 80, boxObj.spriteWidth, boxObj.spriteHeight);
-    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.spriteWidth, boxObj.spriteHeight, -70, 80, boxObj.spriteWidth, boxObj.spriteHeight);
-    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.spriteWidth, boxObj.spriteHeight, -140, 80, boxObj.spriteWidth, boxObj.spriteHeight);
-    ctx.drawImage(box2SpriteSheet, box2Obj.srcX, box2Obj.srcY, box2Obj.spriteWidth, box2Obj.spriteHeight, 10, -55, box2Obj.spriteWidth, box2Obj.spriteHeight);
+    ctx.drawImage(playerSpriteSheet, playerObj.srcX, playerObj.srcY, playerObj.frameWidth, playerObj.frameHeight, -150, -175, playerObj.frameWidth, playerObj.frameHeight);
+    ctx.drawImage(bricksSpriteSheet, bricksObj.srcX, bricksObj.srcY, bricksObj.frameWidth, bricksObj.frameHeight, -160, -55, bricksObj.frameWidth, bricksObj.frameHeight);
+    ctx.drawImage(bricksSpriteSheet, bricksObj.srcX, bricksObj.srcY, bricksObj.frameWidth, bricksObj.frameHeight, 100, -55, bricksObj.frameWidth, bricksObj.frameHeight);
+    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.frameWidth, boxObj.frameHeight, -4, 80, boxObj.frameWidth, boxObj.frameHeight);
+    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.frameWidth, boxObj.frameHeight, 65, 80, boxObj.frameWidth, boxObj.frameHeight);
+    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.frameWidth, boxObj.frameHeight, -70, 80, boxObj.frameWidth, boxObj.frameHeight);
+    ctx.drawImage(boxSpriteSheet, boxObj.srcX, boxObj.srcY, boxObj.frameWidth, boxObj.frameHeight, -140, 80, boxObj.frameWidth, boxObj.frameHeight);
+    ctx.drawImage(box2SpriteSheet, box2Obj.srcX, box2Obj.srcY, box2Obj.frameWidth, box2Obj.frameHeight, 10, -55, box2Obj.frameWidth, box2Obj.frameHeight);
     ctx.restore();
 
     playerObj.framesDrawn++;
@@ -128,8 +120,8 @@ function animate() {
 
 function resizeImage() {
     let scaleFactor = 1;
-    let midXPos = innerWidth / 2 - (playerObj.spriteWidth * scaleFactor) / 2;
-    let midYPos = innerHeight / 2 - (playerObj.spriteHeight * scaleFactor) / 2;
+    let midXPos = innerWidth / 2 - (playerObj.frameWidth * scaleFactor) / 2;
+    let midYPos = innerHeight / 2 - (playerObj.frameHeight * scaleFactor) / 2;
     ctx.translate(midXPos, midYPos);
     ctx.scale(scaleFactor, scaleFactor);
 
@@ -137,19 +129,19 @@ function resizeImage() {
 
 addEventListener("keydown", e => {
     if(e.key === "ArrowLeft"){
-        playerObj.srcY = 1 * playerObj.spriteHeight;
+        playerObj.srcY = 1 * playerObj.frameHeight;
     }
 });
 
 addEventListener("keydown", e => {
     if(e.key === "ArrowRight"){
-        playerObj.srcY = 0 * playerObj.spriteHeight;
+        playerObj.srcY = 0 * playerObj.frameHeight;
     }
 });
 
 //So the canvas can't be rendered before the image
 let numOfImages = 1;
 function loadImages() {
-    if(--numOfImages > 0) return;
+    if(--numOfImages < 0) return;
     animate();
 }
